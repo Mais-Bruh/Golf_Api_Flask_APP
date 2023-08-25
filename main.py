28e7ebf9d6d0c6bfe041a47581b60b012b67795b
nicholaswhite <engineeredbynick@gmail.com>
2023-08-25 17:50:10 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    url = "https://live-golf-data.p.rapidapi.com/stats"
+    querystring = {
+        "year": "2022",
+        "statId": "186"
+    }
+    headers = {
+        "X-RapidAPI-Key": "770b2eb52cmsha43630f51417db0p1cba27jsndc616789d8fc",  # Replace with your API key
+        "X-RapidAPI-Host": "live-golf-data.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    letter = request.args.get('letter', '').upper()  # Get the submitted letter and convert to uppercase
+
+    player_data = []
+    for player in data['rankings']:
+        player_name = f"{player['firstName']} {player['lastName']}"
+        player_info = {
+            'name': player_name,
+            'rank': player['rank']['$numberInt'],
+            'events': player['events']['$numberInt'],
+            'total_points': player['totalPoints']['$numberDouble']
+        }
+        if not letter or player_name.startswith(letter):
+            player_data.append(player_info)
+
+    return render_template('index.html', player_data=player_data, letter=letter)
+
+if __name__ == '__main__':
+    app.run(debug=True)
